4fd57a59e7ec6b945a2c2c743042a7d0b1587bcb
itgod520 <1409260752@qq.com>
2025-03-28 00:47:28 +0800

    判断奇偶

---
+++ Test2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             判断奇偶
+             */
+            Console.WriteLine("请输入一个数：");
+            string num=Console.ReadLine();
+            Console.WriteLine(int.Parse(num)%2);
+        }
+    }
+}
