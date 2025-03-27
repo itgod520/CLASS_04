04222c689d87bcfba3d85f32eab706e8cd516fac
itgod520 <1409260752@qq.com>
2025-03-28 00:36:10 +0800

    第一次修改

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -10,7 +10,18 @@ namespace Test1
     {
         static void Main(string[] args)
         {
+            
+            string num1=Console.ReadLine();
 
+            //if (num1==("10"))
+            //{
+            //    Console.WriteLine("888888888");
+            //}
+
+            if (num1.Equals("10"))
+            {
+                Console.WriteLine("888888888");
+            }
         }
     }
 }
