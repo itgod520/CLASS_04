1898b5962bca105bc8f6f10cb3a4eb42eda25c28
itgod520 <1409260752@qq.com>
2025-03-28 00:13:57 +0800

    添加项目文件。

---
+++ CLASS_04/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLASS_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==========请输入两个数值==========");
+            Console.WriteLine("输入第一个数值：");
+            string num1=Console.ReadLine();
+            Console.WriteLine("请输入第二个数值：");
+            string num2=Console.ReadLine();
+            Console.WriteLine($"{num1+num2}");
+
+            Console.WriteLine(num1+num2);
+
+            Console.WriteLine(int.Parse(num1)+int.Parse(num2));
+        }
+    }
+}
